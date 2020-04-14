2297f777a140b48b15147aeaf19c1e0b5b1977c1
nikhil5bhai <nikhil5bhai@yahoo.co.in>
2020-04-14 15:40:29 -0500

    Added Test Class - DriverManagerTry

---
+++ MyArtifactId/src/test/java/com/nikhil/grp/MyArtifactId/WebDriverManagerTry.java
@@ -0,0 +1,27 @@
+package com.nikhil.grp.MyArtifactId;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WebDriverManagerTry {
+	
+	public WebDriver driver;
+	
+	@Test (priority =1)
+	public void setupTest() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();		
+	}
+	
+	@Test (priority =2)
+	public void openGoogleTest() {
+		driver.get("http://www.google.com/");
+		String expectedPageTitle = "Google";
+		Assert.assertTrue(driver.getTitle().contains(expectedPageTitle), "Title isnt matching");
+	}
+
+}
